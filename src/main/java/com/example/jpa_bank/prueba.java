2f8fa85e4cd1dd957c2430377984afea501c5a84
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-03-22 13:52:59 -0500

    metodoxd

---
+++ src/main/java/com/example/jpa_bank/prueba.java
@@ -0,0 +1,15 @@
+package com.example.jpa_bank;
+
+public class prueba {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    public void xd()
+    {
+        if (true)
+        {
+
+        }
+    }
+}
